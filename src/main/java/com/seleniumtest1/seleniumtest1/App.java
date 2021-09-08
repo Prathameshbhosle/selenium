d9b26566729918c378361819ab3512749c99e786
Prathameshbhosle <86553395+Prathameshbhosle@users.noreply.github.com>
2021-09-08 18:01:13 +0530

    first commit

---
+++ src/main/java/com/seleniumtest1/seleniumtest1/App.java
@@ -0,0 +1,13 @@
+package com.seleniumtest1.seleniumtest1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/seleniumtest1/seleniumtest1/NewTest.java
@@ -0,0 +1,20 @@
+package com.seleniumtest1.seleniumtest1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
+
